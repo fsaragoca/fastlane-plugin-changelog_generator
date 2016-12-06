702aa840d10c3098c41ed9bc91800c25c7dc27b2
Fernando Saragoca <fsaragoca@me.com>
2016-12-06 14:22:25 -0200

    Removing broken test

--- spec/changelog_generator_action_spec.rb
+++ spec/changelog_generator_action_spec.rb
@@ -1,9 +1,4 @@
-describe Fastlane::Actions::ChangelogGeneratorAction do
+describe Fastlane::Actions::GenerateChangelogAction do
   describe '#run' do
-    it 'prints a message' do
-      expect(Fastlane::UI).to receive(:message).with("The changelog_generator plugin is working!")
-
-      Fastlane::Actions::ChangelogGeneratorAction.run(nil)
-    end
   end
 end
