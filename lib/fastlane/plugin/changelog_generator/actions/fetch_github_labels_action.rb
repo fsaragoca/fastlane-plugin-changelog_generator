7e80819a7bfc772240964461ccbc7498ac348eed
Fernando Saragoca <fsaragoca@me.com>
2016-12-07 11:22:04 -0200

    Actions refactor

---
+++ lib/fastlane/plugin/changelog_generator/actions/fetch_github_labels_action.rb
@@ -0,0 +1,69 @@
+module Fastlane
+  module Actions
+    class FetchGithubLabelsAction < Action
+      module SharedValues
+        GENERATE_CHANGELOG_GITHUB_LABELS = :GENERATE_CHANGELOG_GITHUB_LABELS
+        GENERATE_CHANGELOG_GITHUB_PULL_REQUESTS = :GENERATE_CHANGELOG_GITHUB_PULL_REQUESTS
+      end
+
+      def self.run(params)
+        Actions.verify_gem!('octokit')
+
+        labels = lane_context[SharedValues::GENERATE_CHANGELOG_GITHUB_LABELS]
+        pull_requests = lane_context[SharedValues::GENERATE_CHANGELOG_GITHUB_PULL_REQUESTS]
+        if labels && pull_requests
+          return labels, pull_requests
+        end
+
+        api_token = params[:github_api_token]
+        base_branch = params[:base_branch]
+        project = params[:github_project]
+
+        labels, pull_requests = Helper::ChangelogGeneratorFetcher.fetch_labels(project,
+                                                                               base_branch,
+                                                                               api_token)
+        lane_context[SharedValues::GENERATE_CHANGELOG_GITHUB_LABELS] = labels
+        lane_context[SharedValues::GENERATE_CHANGELOG_GITHUB_PULL_REQUESTS] = pull_requests
+        return labels, pull_requests
+      end
+
+      def output
+        [
+          ['GENERATE_CHANGELOG_GITHUB_LABELS', 'Fetched GitHub labels'],
+          ['GENERATE_CHANGELOG_GITHUB_PULL_REQUESTS', 'Fetched GitHub pull requests']
+        ]
+      end
+
+      def self.description
+        "Fetches GitHub for labels & merged pull requests"
+      end
+
+      def self.authors
+        ["Fernando Saragoca"]
+      end
+
+      def self.return_value
+        "A tuple with an array of labels & an array of pull requests"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :github_project,
+                                       env_name: 'GENERATE_CHANGELOG_GITHUB_PROJECT',
+                                       description: 'GitHub project name, including organization'),
+          FastlaneCore::ConfigItem.new(key: :github_api_token,
+                                       env_name: 'GENERATE_CHANGELOG_GITHUB_API_TOKEN',
+                                       description: 'API token to access GitHub API',
+                                       default_value: ENV["GITHUB_API_TOKEN"]),
+          FastlaneCore::ConfigItem.new(key: :base_branch,
+                                       env_name: 'GENERATE_CHANGELOG_BASE_BRANCH',
+                                       description: 'Base branch for pull requests')
+        ]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
---
+++ lib/fastlane/plugin/changelog_generator/actions/generate_release_changelog_action.rb
@@ -0,0 +1,93 @@
+module Fastlane
+  module Actions
+    class GenerateReleaseChangelogAction < Action
+      def self.run(params)
+        tag_a = params[:tag_a] || prompt_tag("Please enter first tag: ")
+        tag_b = params[:tag_b]
+        if tag_b.nil? && params[:skip_tag_b].nil?
+          tag_b = prompt_tag("Please enter second tag: ", [tag_a], true)
+        end
+
+        labels, pull_requests = other_action.fetch_github_labels(github_project: params[:github_project],
+                                                                 base_branch: params[:base_branch],
+                                                                 github_api_token: params[:github_api_token])
+
+        release = Helper::ChangelogGeneratorRelease.new(labels, pull_requests, tag_b, tag_a)
+        Helper::ChangelogGeneratorRender.new([release], labels, params).to_markdown
+      end
+
+      def self.prompt_tag(message = '', excluded_tags = [], allow_none = false)
+        no_tag_text = 'No tag'
+
+        available_tags = `git tag --sort=taggerdate`.split("\n").reverse.first(10)
+        available_tags.reject! {|tag| excluded_tags.include?(tag)}
+        available_tags << no_tag_text if allow_none
+
+        if allow_none && available_tags.count == 1
+          UI.important("Skiping second tag because there are no tags available.")
+          return nil
+        end
+
+        tag = UI.select(message, available_tags)
+        tag = nil if tag == no_tag_text
+        tag
+      end
+
+      def self.description
+        "Changelog generation based on merged pull requests & tags, filtered by one or two tags"
+      end
+
+      def self.authors
+        ["Fernando Saragoca"]
+      end
+
+      def self.return_value
+        "Generated changelog"
+      end
+
+      def self.available_options
+        [
+          FastlaneCore::ConfigItem.new(key: :github_project,
+                                       env_name: 'GENERATE_CHANGELOG_GITHUB_PROJECT',
+                                       description: 'GitHub project name, including organization'),
+          FastlaneCore::ConfigItem.new(key: :github_api_token,
+                                       env_name: 'GENERATE_CHANGELOG_GITHUB_API_TOKEN',
+                                       description: 'API token to access GitHub API',
+                                       default_value: ENV["GITHUB_API_TOKEN"]),
+          FastlaneCore::ConfigItem.new(key: :base_branch,
+                                       env_name: 'GENERATE_CHANGELOG_BASE_BRANCH',
+                                       description: 'Base branch for pull requests'),
+          FastlaneCore::ConfigItem.new(key: :template,
+                                       env_name: 'GENERATE_CHANGELOG_TEMPLATE',
+                                       description: 'Template for generating changelog',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :template_path,
+                                       env_name: 'GENERATE_CHANGELOG_TEMPLATE_PATH',
+                                       description: 'Contents of path will override `template` param',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :tag_a,
+                                       env_name: 'GENERATE_CHANGELOG_TAG_A',
+                                       description: 'Tag to filter pull requests',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :tag_b,
+                                       env_name: 'GENERATE_CHANGELOG_TAG_B',
+                                       description: 'Tag to filter pull requests',
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :skip_tag_b,
+                                       env_name: 'GENERATE_CHANGELOG_SKIP_TAG_B',
+                                       description: 'Skip second tag',
+                                       is_string: false,
+                                       optional: true),
+          FastlaneCore::ConfigItem.new(key: :output_path,
+                                       env_name: 'GENERATE_CHANGELOG_OUTPUT_PATH',
+                                       description: 'If set, will automatically write changelog to output path',
+                                       optional: true)
+        ]
+      end
+
+      def self.is_supported?(platform)
+        true
+      end
+    end
+  end
+end
